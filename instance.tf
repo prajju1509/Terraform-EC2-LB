09c9e790ef0782e04b36975c965d96452ec2933b
Prajwal BM <117504878+prajju1509@users.noreply.github.com>
2023-01-07 08:37:29 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0b0dcb5067f052a63"
+  ami = "ami-0f69bc5520884278e"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet1.id
   user_data = file("shell.sh")
@@ -9,7 +9,7 @@ resource "aws_instance" "ec2" {
   }
 }
 resource "aws_instance" "ec21" {
-  ami = "ami-0b0dcb5067f052a63"
+  ami = "ami-0f69bc5520884278e"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet1.id
   user_data = file("shell.sh")
