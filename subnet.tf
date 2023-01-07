d168830c4003372741d1d28b15356025e9281e79
Prajwal BM <117504878+prajju1509@users.noreply.github.com>
2023-01-07 08:33:09 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnet1" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
   cidr_block = "10.0.0.0/25"
   tags = {
@@ -9,7 +9,7 @@ resource "aws_subnet" "subnet1" {
 }
 resource "aws_subnet" "subnet2" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
   map_public_ip_on_launch = true
   cidr_block = "10.0.0.128/25"
   tags = {
